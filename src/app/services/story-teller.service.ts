9a028843560f7944ea6b34c669d29edeb0579da2
Sung Yoo <yoowook1207@gmail.com>
2023-07-06 16:01:46 -0400

    init

--- src/app/services/story-teller.service.ts
+++ src/app/services/story-teller.service.ts
@@ -9,7 +9,9 @@ import {Observable} from "rxjs";
 })
 export class StoryTellerService {
   serverURL = environment.backendURL
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {
+
+  }
 
   postGptResponse(chatInput:any): Observable<textInputDto> {
     return this.http.post<textInputDto>(`${this.serverURL}/story-teller/plot`, chatInput)
