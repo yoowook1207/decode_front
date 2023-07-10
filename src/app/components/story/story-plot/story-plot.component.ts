597b91f4079175dc7437e158e8e5d6f4d4cc28c7
Sung Yoo <yoowook1207@gmail.com>
2023-07-10 10:01:36 -0400

    decode mock up application

--- src/app/components/story/story-plot/story-plot.component.ts
+++ src/app/components/story/story-plot/story-plot.component.ts
@@ -1,13 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {StoryTellerService} from "../../../services/story-teller.service";
-
+import {inputTextDto} from "../../../services/interfaces/textDto";
+export enum Role {
+  ASSISTANT = 'assistant',
+  USER = 'user'
+}
 @Component({
   selector: 'app-story-plot',
   templateUrl: './story-plot.component.html',
   styleUrls: ['./story-plot.component.scss']
 })
 export class StoryPlotComponent implements OnInit {
+
+  @ViewChild('chatBox', {static:false}) chatBox: ElementRef;
 
   userInput!: FormGroup;
   userMessage: string[] = [];
@@ -19,7 +25,8 @@ export class StoryPlotComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private storyTellerService: StoryTellerService
+    private storyTellerService: StoryTellerService,
+    private renderer: Renderer2
   ) {}
 
   ngOnInit() {
@@ -28,26 +35,51 @@ export class StoryPlotComponent implements OnInit {
     })
   }
 
-  onSubmit() {
-    console.log(this.textInput.value)
+  sendMessage() {
+    // const chatInput: string = this.textInput.value
+    const chatInput: string = this.userInput.get('textInput')?.value
+    this.userMessage.push(chatInput)
+
+    // const postBody: inputTextDto = {
+    //   userMessages: this.userMessage,
+    //   assistantMessages: this.assistantMessage
+    // }
+
+    this.addMessageElement(this.chatBox.nativeElement, chatInput, Role.USER)
+
+    this.chatBox.nativeElement.scrollTop = this.chatBox.nativeElement.scrollHeight
+    // this.storyTellerService.postGptResponse(postBody).subscribe(
+    //   (res) => {
+    //     console.log(res)
+    //     this.addMessageElement(this.chatBox.nativeElement, res.assistant, Role.ASSISTANT)
+    //
+    //     this.assistantMessage.push(res.assistant)
+    //
+    //     this.chatBox.nativeElement.scrollTop = this.chatBox.nativeElement.scrollHeight
+    //   }
+    // )
     this.textInput.setValue('')
   }
 
-  sendMessage() {
-    const chatInput:string = this.textInput.value
-    this.userMessage.push(chatInput)
+  addMessageElement (parentNode:ElementRef, inputText:string, role:Role) {
+    const returnTextDom = this.renderer.createText(inputText)
+    const message = this.renderer.createElement('p')
+    this.renderer.appendChild(message, returnTextDom)
+    this.renderer.addClass(message, `${role}-message`)
+    // user-message
 
-    const myDateTime = '1993-12-07'
-    const postBody = {
-      myDateTime: myDateTime,
-      userMessages: this.userMessage,
-      assistantMessages: this.assistantMessage
+    const messageContainer = this.renderer.createElement('div')
+    this.renderer.addClass(messageContainer, `${role}-message-container`)
+
+    if (role == Role.ASSISTANT) {
+      const aiName = this.renderer.createText('Jason')
+      const nameP = this.renderer.createElement('p')
+      this.renderer.appendChild(nameP, aiName)
+      this.renderer.addClass(nameP, 'ai-name')
+      this.renderer.appendChild(messageContainer, nameP)
     }
 
-    this.storyTellerService.postGptResponse(postBody).subscribe(
-      console.log
-    )
-    this.textInput.setValue('')
-
+    this.renderer.appendChild(messageContainer, message)
+    this.renderer.appendChild(parentNode, messageContainer)
   }
 }
---
+++ src/app/services/interfaces/textDto.ts
@@ -0,0 +1,8 @@
+export interface returnTextDto {
+ assistant: string
+}
+
+export interface inputTextDto {
+  userMessages: string[],
+  assistantMessages: string[],
+}
